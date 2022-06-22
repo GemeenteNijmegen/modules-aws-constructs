ce6a19a74f1f41ad5b5831f42f41db5e86477d78
Marnix Dessing <m.dessing@nijmegen.nl>
2022-06-22 17:16:17 +0200

    feat: use aws cdk construct library

--- .projenrc.js
+++ .projenrc.js
@@ -1,24 +1,19 @@
-const { typescript } = require('projen');
+const { awscdk } = require('projen');
 const { NpmAccess } = require('projen/lib/javascript');
 
 const projectName = '@gemeentenijmegen/aws-constructs';
 
-const project = new typescript.TypeScriptProject({
+const project = new awscdk.AwsCdkConstructLibrary({
+  cdkVersion: '2.28.0',
+  author: 'Gemeente Nijmegen',
+  repository: 'https://github.com/GemeenteNijmegen/modules-aws-constructs.git',
   name: projectName,
-  license: 'EUPL-1.2',
   description: 'AWS CKD constructs for Gemeente Nijmegen',
   defaultReleaseBranch: 'main',
   packageName: projectName,
   release: true,
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
-  deps: [
-    'aws-cdk-lib',
-    'constructs',
-  ],
-  peerDeps: [
-    'aws-cdk-lib',
-    'constructs',
-  ],
+  bundledDeps: ["@types/jest@27.4.1"], // TODO: work around https://github.com/projen/projen/issues/1941
 });
 project.synth();
