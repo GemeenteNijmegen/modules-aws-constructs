29c6d69dd7f56dfecd9c8fa96705e0d270d6d220
Marnix Dessing <m.dessing@nijmegen.nl>
2024-04-11 14:22:40 +0200

    fix: test new property

--- test/dlq.test.ts
+++ test/dlq.test.ts
@@ -1,6 +1,7 @@
 import { App, Duration, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { Key } from 'aws-cdk-lib/aws-kms';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import { DeadLetterQueue } from '../src/DeadLetterQueue';
 
@@ -48,4 +49,28 @@ test('Constrcution of dlq without alarm', () => {
   const alarms = template.findResources('AWS::CloudWatch::Alarm');
   expect(Object.keys(queues)).toHaveLength(1);
   expect(Object.keys(alarms)).toHaveLength(0);
+});
+
+
+test('Construct with bring your own dlq', () => {
+  class DummyStack extends Stack {
+    constructor(scope: Construct, id: string) {
+      super(scope, id);
+      const key = new Key(this, 'key');
+      const mydlq = new Queue(this, 'queue');
+      new DeadLetterQueue(this, 'dlq', {
+        dlq: mydlq,
+        kmsKey: key,
+        alarm: true,
+      });
+    }
+  }
+  const app = new App();
+  const stack = new DummyStack(app, 'dummy-stack');
+  const template = Template.fromStack(stack);
+
+  const queues = template.findResources('AWS::SQS::Queue');
+  const alarms = template.findResources('AWS::CloudWatch::Alarm');
+  expect(Object.keys(queues)).toHaveLength(1);
+  expect(Object.keys(alarms)).toHaveLength(1);
 });
