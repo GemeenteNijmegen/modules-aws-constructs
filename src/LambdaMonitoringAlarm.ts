92025782eb160a4b134412e678bc768d9be7f247
Wieteke <149399985+WietekeNijmegen@users.noreply.github.com>
2024-04-18 18:41:11 +0200

    fix: build errors expect readonly interface properties

--- src/LambdaMonitoringAlarm.ts
+++ src/LambdaMonitoringAlarm.ts
@@ -32,7 +32,7 @@ export interface LamdbaMonitoringAlarmProps {
   /**
    *  All the optional properties for the errorRateMetric and Alarm
    */
-  errorRateProps?: {
+  readonly errorRateProps?: {
     /**
      * The filterpattern used
      * @default FilterPattern.anyTerm('ERROR')
