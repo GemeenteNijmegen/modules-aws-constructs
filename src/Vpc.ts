103e137115ab939249ff41940900ae125e1b60a4
Marnix Dessing <m.dessing@nijmegen.nl>
2024-08-28 15:21:42 +0200

    feat(vpc): Use new landingzone sandbox parameter setup

--- src/Vpc.ts
+++ src/Vpc.ts
@@ -3,14 +3,9 @@ import { IVpc, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
-export interface GemeenteNijmegenVpcProps {
-  isSandbox?: boolean;
-}
-
 export class GemeenteNijmegenVpc extends Construct {
 
   readonly vpc: IVpc;
-  private readonly props?: GemeenteNijmegenVpcProps;
 
   /**
    * Construct to import a VPC using Gemeente Nijmegen
@@ -18,18 +13,12 @@ export class GemeenteNijmegenVpc extends Construct {
    * @param scope
    * @param id
    */
-  constructor(scope: Construct, id: string, props?: GemeenteNijmegenVpcProps) {
+  constructor(scope: Construct, id: string) {
     super(scope, id);
-    this.props = props;
     this.vpc = this.setupVpc();
   }
 
-
   private setupVpc() {
-    // Somehow our landingzone uses the platform unitex prefix in sandboxes...
-    const prefix = this.props?.isSandbox ? 'platformunited' : 'landingzone';
-    const param = `/${prefix}/landing-zone/vpc/route-table-public-subnets-id`;
-    const publicSubnetRouteTableIds = Array(3).fill(StringParameter.valueForStringParameter(this, param));
 
     //VPC setup for ECS cluster
     const vpc = Vpc.fromVpcAttributes(this, 'vpc', {
@@ -39,7 +28,7 @@ export class GemeenteNijmegenVpc extends Construct {
       privateSubnetRouteTableIds: [1, 2, 3].map(i => StringParameter.valueForStringParameter(this, `/landingzone/vpc/route-table-private-subnet-${i}-id`)),
       privateSubnetIds: [1, 2, 3].map(i => StringParameter.valueForStringParameter(this, `/landingzone/vpc/private-subnet-${i}-id`)),
       // Public subnets
-      publicSubnetRouteTableIds: publicSubnetRouteTableIds,
+      publicSubnetRouteTableIds: Array(3).fill(StringParameter.valueForStringParameter(this, '/landingzone/landing-zone/vpc/route-table-public-subnets-id')),
       publicSubnetIds: [1, 2, 3].map(i => StringParameter.valueForStringParameter(this, `/landingzone/vpc/public-subnet-${i}-id`)),
       // Data subnets
       isolatedSubnetRouteTableIds: [1, 2, 3].map(i => StringParameter.valueForStringParameter(this, `/landingzone/vpc/route-table-data-subnet-${i}-id`)),
