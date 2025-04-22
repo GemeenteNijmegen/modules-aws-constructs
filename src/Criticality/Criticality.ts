b2ab4825f636b7fad504e228f4b65db47def9706
Marnix Dessing <m.dessing@nijmegen.nl>
2025-04-22 16:00:59 +0200

    docs: add jsdocs for criticality class

--- src/Criticality/Criticality.ts
+++ src/Criticality/Criticality.ts
@@ -11,14 +11,28 @@ export class Criticality {
     this.lvl = lvl;
   }
 
+  /**
+   * Creates the appropriate suffix for alarms to be picked up
+   * by the landingzone monitoring.
+   * @returns - Alarm suffix (e.g. -low-lvl or -critical-lvl).
+   */
   alarmSuffix() {
     return `-${this.lvl}-lvl`;
   }
 
+  /**
+   * Convert the level to a string.
+   * @returns - E.g. 'high' or 'medium'
+   */
   toString() {
     return this.lvl;
   }
 
+  /**
+   * Increases the criticality by one.
+   * Can be used as: `criticality.increase().alarmSuffix()`
+   * @returns - this object
+   */
   increase() {
     if (this.lvl == 'low') {
       return new Criticality('medium');
@@ -32,6 +46,11 @@ export class Criticality {
     return this;
   }
 
+  /**
+   * Decreases the criticality by one.
+   * Can be used as: `criticality.decrease().alarmSuffix()`
+   * @returns - a new criticality object
+   */
   decrease() {
     if (this.lvl == 'critical') {
       return new Criticality('high');
