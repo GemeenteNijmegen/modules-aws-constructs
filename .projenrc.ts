d8f2a1d58cf23521b3f09050095cea357052ecb2
WietekeNijmegen <wietekenijmegen@gmail.com>
2026-07-07 12:12:09 +0200

    chore: upgrades to ts npm and dependencies upgraded

---
+++ .projenrc.ts
@@ -0,0 +1,26 @@
+import { GemeenteNijmegenCdkLib } from '@gemeentenijmegen/projen-project-type';
+
+const projectName = '@gemeentenijmegen/aws-constructs';
+
+const project = new GemeenteNijmegenCdkLib({
+  cdkVersion: '2.28.0',
+  constructsVersion: '10.6.0',
+  author: 'Gemeente Nijmegen',
+  authorAddress: 'devops@nijmegen.nl',
+  repository: 'https://github.com/GemeenteNijmegen/modules-aws-constructs.git',
+  repositoryUrl: 'https://github.com/GemeenteNijmegen/modules-aws-constructs.git',
+  projenrcTs: true,
+  name: projectName,
+  description: 'AWS CKD constructs for Gemeente Nijmegen',
+  defaultReleaseBranch: 'main',
+  packageName: projectName,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
+  deps: [
+    '@gemeentenijmegen/projen-project-type',
+  ],
+});
+project.synth();
